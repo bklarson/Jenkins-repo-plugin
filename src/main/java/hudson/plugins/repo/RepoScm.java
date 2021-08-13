abioteau/repo-plugin

Add buildEnvironment method to propagate information from SCM to builds

src/main/java/hudson/plugins/repo/RepoScm.java
@@ -1100,6 +1100,31 @@ public class RepoScm extends SCM implements Serializable {
 		return true;
 	}
 
+	/**
+	 * Adds environmental variables for the builds to the given map.
+	 */
+	@Override
+	public void buildEnvironment(
+			@Nonnull final Run<?, ?> build,
+			@Nonnull final java.util.Map<String, String> env) {
+		final Job<?, ?> job = build.getParent();
+		final EnvVars jobEnv = getEnvVars(null, job);
+
+		final String expandedManifestUrl = jobEnv.expand(manifestRepositoryUrl);
+		final String expandedManifestBranch = jobEnv.expand(manifestBranch);
+		final String expandedManifestFile = jobEnv.expand(manifestFile);
+
+		final List<RevisionState> stateList = build.getActions(RevisionState.class);
+		for (RevisionState state : stateList) {
+			if (state != null
+					&& isRelevantState(state, expandedManifestUrl,
+							expandedManifestBranch, expandedManifestFile)) {
+				env.put("REPO_MANIFEST", state.getManifest());
+				break;
+			}
+		}
+	}
+
 	private String getStaticManifest(final Launcher launcher,
 			final FilePath workspace, final OutputStream logger,
 			final EnvVars env)

src/main/resources/hudson/plugins/repo/RepoScm/buildEnv.groovy
@@ -1,0 +1,9 @@
+package hudson.plugins.repo.RepoSCM
+
+def l = namespace(lib.JenkinsTagLib)
+
+['REPO_MANIFEST'].each {name ->
+    l.buildEnvVar(name: name) {
+        raw(_("${name}.blurb"))
+    }
+}

bklarson/Jenkins-repo-plugin

src/main/java/hudson/plugins/repo/RepoScm.java
@@ -1111,6 +1111,31 @@ public class RepoScm extends SCM implements Serializable {
 		return true;
 	}
 
+	/**
+	 * Adds environmental variables for the builds to the given map.
+	 */
+	@Override
+	public void buildEnvironment(
+			@Nonnull final Run<?, ?> build,
+			@Nonnull final java.util.Map<String, String> env) {
+		final Job<?, ?> job = build.getParent();
+		final EnvVars jobEnv = getEnvVars(null, job);
+
+		final String expandedManifestUrl = jobEnv.expand(manifestRepositoryUrl);
+		final String expandedManifestBranch = jobEnv.expand(manifestBranch);
+		final String expandedManifestFile = jobEnv.expand(manifestFile);
+
+		final List<RevisionState> stateList = build.getActions(RevisionState.class);
+		for (RevisionState state : stateList) {
+			if (state != null
+					&& isRelevantState(state, expandedManifestUrl,
+							expandedManifestBranch, expandedManifestFile)) {
+				env.put("REPO_MANIFEST_XML", state.getManifest());
+				break;
+			}
+		}
+	}
+
 	private String getStaticManifest(final Launcher launcher,
 			final FilePath workspace, final OutputStream logger,
 			final EnvVars env)

src/main/resources/hudson/plugins/repo/RepoScm/buildEnv.groovy
@@ -1,0 +1,9 @@
+package hudson.plugins.repo.RepoSCM
+
+def l = namespace(lib.JenkinsTagLib)
+
+['REPO_MANIFEST_XML'].each {name ->
+    l.buildEnvVar(name: name) {
+        raw(_("${name}.blurb"))
+    }
+}